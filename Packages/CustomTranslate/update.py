2bb5685419ea5951baa48e90ad038ce03f7e2b67
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-06-29 10:08:15 -0300

    v0.23.06.29

---
+++ Packages/CustomTranslate/update.py
@@ -0,0 +1,59 @@
+#pip install panda
+#pip install ipython
+
+import csv
+import pandas as pd
+from IPython.display import display
+import os
+import math
+import numpy as np
+
+translates = pd.read_csv("customTranslate.csv",sep=";")
+linhas = len(translates)
+path = '/var/www/dataverse/langBundles/'
+
+for ln in range(linhas):
+    if (ln >= 10):
+        print("Header")
+    else:
+        file = translates.iloc[ln]['file']
+        lang = translates.iloc[ln]['lang']
+        field = translates.iloc[ln]['field']
+        description = translates.iloc[ln]['translate']
+        lang = str(lang)
+        ################# Seleciona arquivo de idioma
+        if (lang != 'nan'):
+            file += '_'+lang
+
+        file += '.properties'
+
+        ####################### Arquivo
+        if os.path.isfile(path + file):
+            print("================== P R O C E S S A N D O ==")
+            print(file)
+            ############################## Lendo arquivo
+            fileN = file+'.new'
+            ################ SOURCE
+            with open(path + file,"r", encoding="utf-8") as source:
+                texto = source.readlines()
+
+            ################ DESTINITY
+            with open(fileN,"w", encoding="utf-8") as destinity:
+                for frase in texto:
+                    if field in frase:
+                        frase = field +"="+description
+                        print("=============",frase)
+                    destinity.writelines(frase)
+
+            ################ GERAR COPIAS
+            i = 1
+            fileO = file+'.bk'
+            while os.path.isfile(fileO):
+                fileO = file + i + '.bk'
+                i += 1
+            print("========"+fileO)
+
+        else:
+            print(" ERRO ao acessar arquivo",file)
+
+print("\nO DataFrame contém {0} linhas".format(linhas))
