22d9add644cb6c57e335fba692fc191e7e347d1b
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-10-10 11:00:41 -0300

    Update install.sh

--- Packages/Aleia/AboutUs/install.sh
+++ Packages/Aleia/AboutUs/install.sh
@@ -2,7 +2,9 @@
 
 export PAYARA=/usr/local/payara5/glassfish
 export SERVER_URL=http://localhost:8080
-export URL=https://aleia.ibict.br/About.hxml
-export ABOUT_XML=Aboutus.xml
+export URL=https://aleia.ibict.br/
+export ABOUT_XML=About_br.xhtml
+export APP=/usr/local/payara5/glassfish/domains/domain1/applications/dataverse-5.12.1/
 
+cp $ABOUT_XML $APP/$ABOUT_XML
 curl -X PUT -d $URL$ABOUT_XML $SERVER_URL/api/admin/settings/:NavbarAboutUrl
