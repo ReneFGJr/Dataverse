69449bc84d9ae14c8a0865524367f7e21a589e9b
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-10-11 14:38:56 -0300

    v0.23.10.11

--- Packages/Aleia/AboutUs/install.sh
+++ Packages/Aleia/AboutUs/install.sh
@@ -6,9 +6,14 @@ export URL=https://aleia.ibict.br/
 export ABOUT_XML=about.xhtml
 export FAQ_XML=faq.xhtml
 export APP=/usr/local/payara5/glassfish/domains/domain1/applications/dataverse-5.12.1/
+export GUIDE=Guia_Aleia.pdf
 
 cp $ABOUT_XML $APP/$ABOUT_XML
 cp $FAQ_XML $APP/$FAQ_XML
+mkdir $APP/assets/
+mkdir $APP/assets/pdf/
+cp $GUIDE $APP/assets/pdf/$GUIDE
+
 curl -X PUT -d $URL$ABOUT_XML $SERVER_URL/api/admin/settings/:NavbarAboutUrl
 
 #Reinicia o Dataverse
