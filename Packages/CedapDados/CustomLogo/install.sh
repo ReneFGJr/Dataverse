7844b26bd5daca86776de2ede0e1e7ba24cfb700
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-10-27 08:20:13 -0300

    v0.23.10.27

--- Packages/CedapDados/CustomLogo/install.sh
+++ Packages/CedapDados/CustomLogo/install.sh
@@ -1,6 +1,6 @@
-export LOGO='logo_cedapdados'
+export LOGO='logo_cedapdados.png'
 mkdir /usr/local/payara5/glassfish/domains/domain1/docroot/logos/
 mkdir /usr/local/payara5/glassfish/domains/domain1/docroot/logos/navbar/
 cp *.png /usr/local/payara5/glassfish/domains/domain1/docroot/logos/navbar/.
 
-curl -X PUT -d '/logos/navbar/$LOGO' http://localhost:8080/api/admin/settings/:LogoCustomizationFile
+curl -X PUT -d '/logos/navbar/logo_cedapdados.png' http://localhost:8080/api/admin/settings/:LogoCustomizationFile
