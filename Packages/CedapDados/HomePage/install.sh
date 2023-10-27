068201f235e448a2eebbd559edf11aa014bd2393
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-10-27 08:29:06 -0300

    V0.23.10.27

--- Packages/CedapDados/HomePage/install.sh
+++ Packages/CedapDados/HomePage/install.sh
@@ -3,7 +3,6 @@
 export PAYARA=/usr/local/payara5/glassfish
 export SERVER_URL=http://localhost:8080
 export APP=/usr/local/payara5/glassfish/domains/domain1/applications/dataverse-5.12.1/
-export IMG=fluxo_deposito.png
 
 export URL=https://cedapdados.ufrgs.br/
 export HOME=welcome.xhtml
@@ -11,6 +10,7 @@ export HOME=welcome.xhtml
 cp $HOME $APP/$HOME
 mkdir $APP/assets/
 mkdir $APP/assets/img/
-cp $IMG $APP/assets/img/$IMG
+cp *.png $APP/assets/img/$IMG
+cp *.jpg $APP/assets/img/$IMG
 
 curl -X PUT -d $APP/$HOME $SERVER_URL/api/admin/settings/:HomePageCustomizationFile
