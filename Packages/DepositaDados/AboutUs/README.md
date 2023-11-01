fc1c6d65a9ba888fb1bfdc0901f7d1a5a73747b0
Henrique <hfr@gmx.co.uk>
2023-11-01 18:09:58 -0300

    Adding Deposita Dados directory

---
+++ Packages/DepositaDados/AboutUs/README.md
@@ -0,0 +1,12 @@
+:NavbarAboutUrl
+Set :NavbarAboutUrl to a fully-qualified URL which will be used for the “About” link in the navbar.
+
+Note: The “About” link will not appear in the navbar until this option is set.
+
+curl -X PUT -d http://dataverse.example.edu http://localhost:8080/api/admin/settings/:NavbarAboutUrl
+
+chmod 700 install.sh
+
+<h1>Guia</h1>
+No arquivo Guia_Aleia.pdf está disponível a última versão do Guia do Usuário do Aleia
+Versão de 11/out./2023.
---
+++ Packages/DepositaDados/AboutUs/install.sh
@@ -0,0 +1,23 @@
+#echo "https://guides.dataverse.org/en/latest/installation/config.html?highlight=apitermsofuse"
+
+export PAYARA=/usr/local/payara5/glassfish
+export SERVER_URL=http://localhost:8080
+export URL=https://aleia.ibict.br/
+export ABOUT_XML=about.xhtml
+export FAQ_XML=faq.xhtml
+export APP=/usr/local/payara5/glassfish/domains/domain1/applications/dataverse-5.12.1/
+export GUIDE=Guia_Aleia.pdf
+export IMG=fluxo_deposito.png
+
+cp $ABOUT_XML $APP/$ABOUT_XML
+cp $FAQ_XML $APP/$FAQ_XML
+mkdir $APP/assets/
+mkdir $APP/assets/pdf/
+mkdir $APP/assets/img/
+cp $GUIDE $APP/assets/pdf/$GUIDE
+cp $IMG $APP/assets/img/$IMG
+
+curl -X PUT -d $URL$ABOUT_XML $SERVER_URL/api/admin/settings/:NavbarAboutUrl
+
+#Reinicia o Dataverse
+/home/dataverse/restart
