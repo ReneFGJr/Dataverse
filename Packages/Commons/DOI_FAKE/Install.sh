a9e07ef6735123f828c39ce6f920467a76d174b0
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-11-16 15:08:23 -0300

    v0.23.11.16

--- Packages/Commons/DOI_FAKE/Install.sh
+++ Packages/Commons/DOI_FAKE/Install.sh
@@ -1,7 +1,3 @@
 
-
-
+echo "Ativa DOI FAKE"
 curl http://localhost:8080/api/admin/settings/:DoiProvider -X PUT -d FAKE_DOI_PROVIDER=true
-
-echo "Atribui DOI para cada dataset, sem gerar para arquivos"
-curl -X PUT -d 'false' http://localhost:8080/api/admin/settings/:FilePIDsEnabled
