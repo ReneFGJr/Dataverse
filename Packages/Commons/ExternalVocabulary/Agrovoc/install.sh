fa52d3aec7a1f0506f8e3149c77f54250373f9e5
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-27 12:00:02 -0300

    A

--- Packages/Commons/ExternalVocabulary/Agrovoc/install.sh
+++ Packages/Commons/ExternalVocabulary/Agrovoc/install.sh
@@ -1,1 +1,5 @@
-curl -X PUT --upload-file keywordUnesco.json http://localhost:8080/api/admin/settings/:CVocConf
+mkdir /usr/local/payara6/glassfish/domains/domain1/applications/dataverse-6.1/cvoc/
+mkdir /usr/local/payara6/glassfish/domains/domain1/applications/dataverse-6.1/cvoc/js
+cp skosmos.js /usr/local/payara6/glassfish/domains/domain1/applications/dataverse-6.1/cvoc/js/.
+cp ../cvocutils.js /usr/local/payara6/glassfish/domains/domain1/applications/dataverse-6.1/cvoc/js/.
+curl -X PUT --upload-file agrovoc.json http://localhost:8080/api/admin/settings/:CVocConf
