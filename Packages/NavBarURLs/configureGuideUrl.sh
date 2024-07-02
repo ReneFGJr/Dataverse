0177346cafe8c9d1ccdc8fedb2168fa1af5b92d9
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-02 16:40:01 -0300

    A

--- Packages/NavBarURLs/configureGuideUrl.sh
+++ Packages/NavBarURLs/configureGuideUrl.sh
@@ -1,4 +1,7 @@
-curl -X PUT -d http://dataverse.example.edu http://localhost:8080/api/admin/settings/:GuidesBaseUrl
+curl -X PUT -d http://fiocruz. http://localhost:8080/api/admin/settings/:GuidesBaseUrl
+
+curl -X PUT -d https://guia.arcadados-beta.fiocruz.br  http://localhost:8080/api/admin/settings/:GuidesBaseUrl
 
 #Guide version
-#curl -X PUT -d 1234-new-feature http://localhost:8080/api/admin/settings/:GuidesVersion
+#curl -X PUT -d '1' http://localhost:8080/api/admin/settings/:GuidesVersion
+#curl -X DELETE http://localhost:8080/api/admin/settings/:GuidesVersion
