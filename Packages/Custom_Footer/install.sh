a64505f3e4e7594241704b1a06deb8bdc1e2cbb1
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-09-16 15:06:05 -0300

    A

--- Packages/Custom_Footer/install.sh
+++ Packages/Custom_Footer/install.sh
@@ -4,4 +4,5 @@ LOGO='footer.png'
 mkdir $PAYARA/domains/domain1/docroot/logos
 mkdir $PAYARA/domains/domain1/docroot/logos/navbar
 cp img/$LOGO $PAYARA/domains/domain1/docroot/logos/.
+cp custom-footer.html /var/www/dataverse/branding/custom-footer.html
 curl -X PUT -d '/var/www/dataverse/branding/custom-footer.html' http://localhost:8080/api/admin/settings/:FooterCustomizationFile
