67a7bb09f45fb038703f92913159e5325b267a3a
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-11-12 09:59:03 -0300

    Update install.sh

--- Packages/Language/install.sh
+++ Packages/Language/install.sh
@@ -12,11 +12,21 @@ $PAYARA/bin/asadmin stop-domain
 
 echo "Enviando Arquivo"
 $PAYARA/bin/asadmin start-domain
+
+clear
+echo "Aguarde o servidor voltar do Dataverse, e pressione [ENTER]"
+read -n 1 -s
+echo "Continuando..."
+
 curl http://localhost:8080/api/admin/datasetfield/loadpropertyfiles -X POST --upload-file languages.zip -H "Content-Type: application/zip"
 $PAYARA/bin/asadmin stop-domain
 
 echo "Definindo os idiomas ativos"
 $PAYARA/bin/asadmin start-domain
+clear
+echo "Aguarde o servidor voltar do Dataverse, e pressione [ENTER]"
+read -n 1 -s
+echo "Continuando..."
 echo "===>Definindo os idiomas do Dataverse e suas extensões"
 curl http://localhost:8080/api/admin/settings/:Languages -X PUT -d '[{"locale":"pt","title":"Português"}, {"locale":"en","title":"English"}, {"locale":"es","title":"Espanhol"}]'
 $PAYARA/bin/asadmin stop-domain
