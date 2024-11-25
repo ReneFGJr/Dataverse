44a4ed31c3b0f5bd410ac74620bee225c948b428
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-11-25 06:44:36 -0300

    Update update_language.py

--- repositories/Aleia/AboutUs/update_language.py
+++ repositories/Aleia/AboutUs/update_language.py
@@ -24,7 +24,7 @@ def update_propriety(fileO,fileS):
         file.writelines(lines)
 
 
-file_path = "/var/www/databerse/Bundle.proprieties"
+file_path = "/var/www/databerse/langBundles/Bundle_pt.properties"
 additional_file_path = "about_pt.propriety"
 update_propriety(file_path,additional_file_path)
 print("Arquivo atualizado com sucesso!")
