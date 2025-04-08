1f3b0413eaea16cde31aa42da7d1f4dcb85e3ded
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-04-08 12:28:23 -0300

    A

--- repositories/DepositaDados/GuiaUsuario/install.sh
+++ repositories/DepositaDados/GuiaUsuario/install.sh
@@ -1,3 +1,17 @@
-#https://depositadados.ibict.br/guide/guiaUsuarioBr.pdf
+#https://aleia.ibict.br/guide/guiaUsuarioBr.pdf
 
+# Defina o caminho da instalação do Payara
+export PAYARA=/usr/local/payara6/glassfish
+echo "PAYARA=$PAYARA"
+
+echo "Copiando guide"
+cp guide.xhtml $PAYARA/domains/domain1/applications/dataverse-6.4/.
+echo "Criando Diretorio"
+mkdir -p $PAYARA/domains/domain1/docroot/guide
+
+echo "Copiando Arquivos"
+cp guiaUsuarioBr.pdf /$PAYARA/domains/domain1/docroot/guide/guiaUsuarioBr.pdf
 cp guiaUsuarioBr.pdf /var/www/guide/public/
+
+echo "Configurando GuidePage"
+curl -X PUT -d /guide.xhtml http://localhost:8080/api/admin/settings/:NavbarGuidesUrl
