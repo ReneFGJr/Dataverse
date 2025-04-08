b47d91db04dcbcb3c5fdcfc5ccf85865ec74b97a
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-04-08 11:22:55 -0300

    A

---
+++ repositories/Aleia/StartPage/install.sh
@@ -0,0 +1,3 @@
+curl -X PUT -d '/var/www/dataverse/branding/custom-header.html' http://localhost:8080/api/admin/settings/:HeaderCustomizationFile
+
+#curl -X PUT -d 'true' http://localhost:8080/api/admin/settings/:DisableRootDataverseThem
