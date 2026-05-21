e1b5a8f390990fc658646cdbfeab0ec00f001850
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-05-21 14:40:43 -0300

    A

---
+++ Packages/Custom_Homepage/image_export.sh
@@ -0,0 +1,7 @@
+# Customização da Logo - versão 0.24.09.12
+# Para a variavel $PAYARA veja Commons/Enviroment
+export LOGO='*.jpg'
+export PAYARA='/usr/local/payara7/glassfish'
+mkdir $PAYARA/domains/domain1/docroot/logos
+mkdir $PAYARA/domains/domain1/docroot/logos/navbar
+cp $LOGO $PAYARA/domains/domain1/docroot/logos/navbar/.
