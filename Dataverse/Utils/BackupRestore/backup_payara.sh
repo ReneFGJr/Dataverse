9d301a4ffa02590486edaeed01bb2d4e596534de
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-06-08 17:23:56 -0300

    Update backup_payara.sh

--- Dataverse/Utils/BackupRestore/backup_payara.sh
+++ Dataverse/Utils/BackupRestore/backup_payara.sh
@@ -1,7 +1,11 @@
-mkdir /home/dataverse/payara
-chown postgres /home/dataverse/payara
-/home/dataverse/stop
+echo "Iniciando backup do Payara Server e do banco de dados dvndb..."
+mkdir /home/dataverse/backup
+chown postgres /home/dataverse/backup
+sudo -u postgres pg_dump dvndb > dvndb_$(date +%Y%m%d).sql
 
+echo "Backup do banco de dados dvndb realizado com sucesso."
+/home/dataverse/stop
+cd /home/dataverse/backup # diretorio onde ficará os arquivos
+export PAYARA=/usr/local/payara6/glashfish
+$PAYARA/bin/asadmin backup-domain --backupdir /home/dataverse/backup domain1
 /home/dataverse/start
-cd /home/dataverse/payara # diretorio onde ficará os arquivos
-$PAYARA/bin/asadmin backup-domain --backupdir /home/dataverse/payara domain1
